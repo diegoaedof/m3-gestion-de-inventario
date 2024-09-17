cdfa437d14284da580b58cddeada5e5b76e408ab
kotorra <bastiankoto@hotmail.com>
2024-09-16 21:12:05 -0300

    creacion estructura

---
+++ main.py
@@ -0,0 +1,26 @@
+inventario={}
+while True:
+	print("Bienvenidos al aCuenta :V")
+ 	print("1. Agregar Producto")
+    print("2. Buscar Producto")
+    print("3. Actualizar Inventario")
+    print("4. Eliminar Producto")
+    print("5. Mostrar Inventario")
+    print("6. Salir")
+    print("------------")
+    opcion=input("Ingrese su opción: ")
+
+	if opcion=="1":
+		producto=input("Ingrese el producto: ")
+	elif opcion=="2":
+		pass
+	elif opcion=="3":
+		pass
+	elif opcion=="4":
+		pass
+	elif opcion=="5":
+		pass
+	elif opcion=="6":
+		break
+	else:
+		print("Error capa 8")
