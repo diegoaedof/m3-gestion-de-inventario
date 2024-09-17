748b595ad50955f61e6725a4adc5a60748eadb52
Cristobal Villarroel <cristobalvillarroelb@outlook.com>
2024-09-17 19:26:27 -0300

    merge 2

--- main.py
+++ main.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-inventario = {}
-
-while True:
-    print("1. Agregar Producto")
-    print("2. Buscar Producto")
-    print("3. Actualizar Inventario")
-    print("4. Eliminar Producto")
-    print("5. Mostrar Inventario")
-    print("6. Salir")
-    print("------------")
-
-    opcion = input("Ingrese su opción: ")
-    if opcion == 6:
-        break
-    
-    if opcion == "1":
-        producto = input("Ingrese el producto: ")
-    elif opcion == "2":
-        pass
-    elif opcion == "3":
-        pass
-    elif opcion == "4":
-        pass
-    elif opcion == "5":
-        pass
-    elif opcion == "6":
-        pass
-    else:
-        print("Error capa 8")
-=======
 # comment
 inventario={}
 while True:
@@ -56,4 +25,3 @@ while True:
 		break
 	else:
 		print("Error capa 8")
->>>>>>> 571ce33384a9c4aa0c4440625881c4ab719cc05f
